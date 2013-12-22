c6d32d84b60951b9c5fcc50aebee065d88923b03
Dave Lafferty <cekolabs@dave-lafferty.com>
2013-12-22 14:46:19 +0000

    Fixed errors upgrading to Django 1.5

--- core/profiles/base_settings.py
+++ core/profiles/base_settings.py
@@ -213,5 +213,11 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
-    }
+    },
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
 }
