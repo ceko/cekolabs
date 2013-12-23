ebf563276559035947a4e2cdf101cfeb9fae9e17
Web Developer <webdev@cheesecake.(none)>
2013-12-22 20:14:10 -0500

    Magicka game is somewhat usable now, can queue up elements and add cards to the board.

--- blog/static/js/finch/core.js
+++ blog/static/js/finch/core.js
@@ -1,25 +1,74 @@
 
 var finch = {}
-finch.game = {
+finch.game = {		
 	//starts a new game, usually called immediately after loading the page.
 	start: function() {
 		//create all uninitialized models
 		finch.game.controls = {
-			elements: new controls.Elements()
+			elements: new models.controls.Elements()
 		};
+		finch.game.hand = new models.Hand();
 		finch.game.views = {
 			global: new views.Global(),
 			elements: new views.Elements({ model:finch.game.controls.elements }),
 			queued_elements: new views.QueuedElements({ model:finch.game.controls.elements }),
+			hand: new views.Hand({ model: finch.game.hand }),
+			hero_summon_points: [
+			     new views.SummonPoint({ model: new models.SummonPoint(), el: $('#hero-side .summon-point:eq(0)') }),
+			     new views.SummonPoint({ model: new models.SummonPoint(), el: $('#hero-side .summon-point:eq(1)') }),
+			     new views.SummonPoint({ model: new models.SummonPoint(), el: $('#hero-side .summon-point:eq(2)') }),
+			     new views.SummonPoint({ model: new models.SummonPoint(), el: $('#hero-side .summon-point:eq(3)') })
+			]
 		};		
+		
+		//testing, add a couple cards.
+		finch.game.hand.draw_card();
+		finch.game.hand.draw_card();
+		finch.game.hand.draw_card();
 	}
 }
 
 /** backbone models **/
 
 /** magicka button combination controls **/
-var controls = {};
-controls.Elements = Backbone.Model.extend({
+var models = { controls: {} };
+
+models.Card = Backbone.Model.extend({
+	initialize: function() {
+		this.set({
+			type: null,
+			title: null,
+			description: null,
+			attributes: [],
+			requirements: [],
+		});
+	},
+	
+	requirements_met: function() {
+		var unused_queued_elements = finch.game.controls.elements.get('queued_elements').slice(0); //clone?
+		var requirements = this.get('requirements');
+		var met_requirements = [];
+		
+		for(i=0;i<requirements.length;i++) {
+			switch(requirements[i].type) {
+				case 'element':
+					var required_element = requirements[i].element;
+					for(var j in unused_queued_elements) {
+						if(unused_queued_elements[j] == required_element) {
+							met_requirements.push(requirements[i]);
+							unused_queued_elements.splice(j,1);
+							break;
+						}
+					}
+					break;
+			}			
+		}
+		
+		return met_requirements.length == requirements.length;
+	}
+}),
+
+models.controls.Elements = Backbone.Model.extend({
 	initialize: function() {
 		this.set({
 			queued_elements: [],	
@@ -42,6 +91,9 @@ controls.Elements = Backbone.Model.extend({
 			['lightning', 'earth'],
 			['lightning', 'water']
 		];
+	},
+	clear_queued_elements: function() {
+		this.set('queued_elements', []);
 	},
 	handle_keydown: function(key) {
 		var element = this.keybindings[key];
@@ -97,6 +149,77 @@ controls.Elements = Backbone.Model.extend({
 		}
 	}
 });
+
+models.Hand = Backbone.Model.extend({
+	
+	initialize: function() {
+		this.set({
+			cards: [],
+		})
+	},
+	
+	remove_card: function(card_model) {
+		var cards = this.get('cards');
+		for(i=0;i<cards.length;i++) {
+			if(cards[i] == card_model) {
+				cards.splice(i,1);
+				this.set('cards', cards);
+				this.trigger('change:cards', this, card_model, 'remove');
+				
+				finch.game.hand.draw_card();				
+				break;
+			}
+		}				
+	},
+	
+	draw_card: function() {
+		var card_model = new models.Card();
+		var card_definition = finch.card_definitions[Math.floor((Math.random()*finch.card_definitions.length))]; 
+		card_model.set('type', card_definition.type);
+		card_model.set('title', card_definition.title);
+		card_model.set('description', card_definition.description);
+		card_model.set('attributes', card_definition.attributes);
+		card_model.set('requirements', card_definition.requirements);
+		
+		var card_view = new views.Card({ model:card_model });
+		card_model.view = card_view;
+		card_view.render();
+				
+		var cards = this.get('cards');
+		cards.push(card_model);
+		this.set('cards', cards);
+		this.trigger('change:cards', this, card_model, 'add');
+	}
+	
+});
+
+models.SummonPoint = Backbone.Model.extend({
+	
+	initialize: function() {
+		this.set({
+			active: true,
+			cards: []
+		});
+	},
+	
+	add_card: function(card_model) {
+		//can accept three types of cards, a wall/shield, a unit, and a ward.
+		//if this is called don't bother checking if it's valid, just add it to the card array.
+		//checks happen before
+		switch(card_model.get('type')) {
+			case 'defensive':
+				this.set('shield', card_model);
+				break;
+		}
+		
+		var cards = this.get('cards');
+		cards.push(card_model);
+		this.set('cards', cards);
+		this.trigger('change:cards', this, card_model, 'add');
+	}
+	
+});
+
 /** end backbone models **/
 
 /** views **/
@@ -106,6 +229,9 @@ views.Global = Backbone.View.extend({
 	events: {
 		'keydown': 'handle_keydown',
 		'keyup': 'handle_keyup'
+	},
+	initialize: function() {
+		this.$el.disableSelection();
 	},
 	handle_keydown: function(evt) {
 		var key = String.fromCharCode(evt.which).toLowerCase();
@@ -136,6 +262,123 @@ views.Elements = Backbone.View.extend({
 		}
 	},
 	
+});
+
+views.Card = Backbone.View.extend({
+	
+	tagName: 'div',
+	className: 'card-wrapper',
+	
+	initialize: function() {
+		finch.game.controls.elements.on("change:queued_elements", this.handle_queued_elements.bind(this));
+		this.draggable_initialized = false;
+		this.$el.data('view', this);
+		this.summoned = false;
+	},
+	
+	handle_summoned: function() {
+		finch.game.hand.remove_card(this.model);
+		finch.game.controls.elements.clear_queued_elements();
+		
+		this.$el.remove();
+		this.summoned = true;		
+	},
+	
+	handle_queued_elements: function(model, queued_elements) {		
+		this.$el.find(".requirements > div").removeClass('fulfilled');
+		for(var index in queued_elements) {
+			var element = queued_elements[index];
+			this.$el.find(".requirements div." + element).not('.fulfilled').first().addClass('fulfilled');
+		}
+		if(this.model.requirements_met()) {							
+			this.$el.find('.card').addClass('requirements-met');			
+			var _this = this;
+			this.$el.draggable({
+				helper: 'clone',
+				appendTo: 'body',
+				containment: '#playing-field',
+				start: function() {
+					$(this).hide();
+				},
+				stop: function() {
+					if(!_this.summoned) $(this).show(); //if we need to revert, else remove.
+				}
+			});
+			this.draggable_initialized = true;			
+			this.$el.draggable('enable');
+		}else{
+			this.$el.find('.card').removeClass('requirements-met');
+			if(this.draggable_initialized &&!this.summoned)
+				this.$el.draggable('disable');
+		}
+	},
+	
+	render: function() {
+		this.$el.html($("#card-template").render({ 
+			type: this.model.get('type'),
+			title: this.model.get('title'),
+			description: this.model.get('description'),
+			attributes: this.model.get('attributes'),
+			requirements: this.model.get('requirements')
+		}));
+		return this;
+	}
+	
+});
+
+views.Hand = Backbone.View.extend({
+	
+	el: $('#card-draw-area'),
+	
+	initialize: function() {
+		this.model.on('change:cards', this.handle_cards_change.bind(this))
+	},
+	
+	handle_cards_change: function(model, card_model, action /*add or delete*/) {
+		//go through cards in the hand and lay them out properly
+		if(action == 'add')
+			this.$el.append(card_model.view.$el);
+		
+		var all_cards = model.get('cards');
+		for(i=0;i<all_cards.length;i++) {
+			all_cards[i].view.$el.animate({
+				left: i*200+20,				
+			});
+		}
+	}
+	
+});
+
+views.SummonPoint = Backbone.View.extend({
+	
+	initialize: function() {
+		this.model.view = this; //should probably make this a standard.
+		var _this = this;
+		this.$el.droppable({
+			accept: ".card-wrapper",
+			hoverClass: "ui-state-active",
+			drop: function( event, ui ) {
+				_this.model.add_card(ui.draggable.data().view.model);
+				//the next line removes the model variable from view.
+				ui.draggable.data().view.handle_summoned(this.model);				
+			}
+		});
+		this.model.on("change:cards", this.handle_cards_changed.bind(this));
+	},
+	
+	handle_cards_changed: function() {
+		this.render();
+	},
+	
+	render: function() {
+		//render units
+		//render shields
+		//render wards
+		
+		var shield = this.model.get('shield');
+		this.$el.html($("#summon-point-template").render({ shield: shield }));
+	}
+
 });
 
 views.QueuedElements = Backbone.View.extend({
@@ -215,5 +458,87 @@ views.QueuedElements = Backbone.View.extend({
 });
 /** end views **/
 
-//instantiate controllers and views here
+//card definitions, goes last because it's long and i don't want to scroll past it every time i change something
+finch.card_definitions = [
+	{
+		type: 'defensive',
+		title: 'Fiery Rock Wall',
+		description: 'A powerful rock wall that does AoE damage when destroyed.',
+		attributes: [
+		   {
+			   type: 'attack',
+			   value: 2			   
+		   },
+		   {
+			   type: 'health',
+			   value: 3
+		   }
+		],
+		requirements: [
+		   {
+			   type: 'element',
+			   element: 'shield'
+		   },
+		   {
+			   type: 'element',
+			   element: 'earth'
+		   },
+		   {
+			   type: 'element',
+			   element: 'fire'
+		   },
+		]
+	},
+	{
+		type: 'defensive',
+		title: 'Strong Rock Wall',
+		description: 'A purely defensive wall good against physical attacks.',
+		attributes: [
+		   {
+			   type: 'health',
+			   value: 5			   
+		   }
+		],
+		requirements: [
+		   {
+			   type: 'element',
+			   element: 'shield'
+		   },
+		   {
+			   type: 'element',
+			   element: 'earth'
+		   },
+		   {
+			   type: 'element',
+			   element: 'earth'
+		   },
+	  	]
+	},
+	{
+		type: 'lightning-aoe',
+		title: 'Arcane Storm',
+		description: 'Does AoE lightning & arcane damage to all enemies on the field.',
+		attributes: [		            
+		   {
+			   type: 'attack',
+			   value: 3			   
+		   }
+		],
+		requirements: [
+		   {
+			   type: 'element',
+			   element: 'lightning'
+		   },
+		   {
+			   type: 'element',
+			   element: 'arcane'
+		   },
+		   {
+			   type: 'element',
+			   element: 'shield'
+		   },
+	  	]
+	}
+];
+
 finch.game.start();
